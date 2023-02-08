293dc2117089e4e2cad5538fc362820c0932626c
admin <admin@YD05-13>
2023-02-08 17:37:26 +0900

    review_until_nextweek

---
+++ homework/src/com/house/review04/StringGame.java
@@ -0,0 +1,36 @@
+package com.house.review04;
+
+public class StringGame {
+	protected char CharData;
+	protected char StrData;
+	
+	
+	public void showCharData() {
+		
+	}
+	
+	public void init() {
+		
+	}
+
+	public char getCharData() {
+		return CharData;
+	}
+
+	public void setCharData(char charData) {
+		CharData = charData;
+	}
+
+	public char getStrData() {
+		return StrData;
+	}
+
+	public void setStrData(char strData) {
+		StrData = strData;
+	}
+
+		
+	
+	
+	
+}
